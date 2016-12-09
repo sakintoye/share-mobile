aceec4f3a1f5dffc6f626d6350e17edbd2bf2386
sakintoye <olasunkanmiakintoye@gmail.com>
2016-12-09 09:28:35 -0500

    Tests

---
+++ Sharepayment/libraries/MaterialLoadingIndicator.swift
@@ -0,0 +1,9 @@
+//
+//  MaterialLoadingIndicator.swift
+//  Sharepayment
+//
+//  Created by Sunkanmi Akintoye Ola on 11/28/16.
+//  Copyright © 2016 Ola. All rights reserved.
+//
+
+import Foundation
